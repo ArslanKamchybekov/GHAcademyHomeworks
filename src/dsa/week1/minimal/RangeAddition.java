772f11d894e967ea3f7c2430c0794950d55a015c
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-07-18 21:50:31 -0500

    Initial commit

---
+++ src/dsa/week1/minimal/RangeAddition.java
@@ -0,0 +1,15 @@
+package dsa.week1;
+
+class Solution {
+    public int[] getModifiedArray(int length, int[][] updates){
+        int[] res = new int[length];
+        for(int[] update: updates){
+            int start = update[0], end = update[1], val = update[3];
+
+            res[start] += val;
+            if(end + 1 < length) res[end + 1] -= val;
+        }
+        for(int i = 1; i < res.length; i++) res[i] += res[i - 1];
+        return res;
+    }
+}
